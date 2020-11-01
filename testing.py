482012ba2638c9362ab9818b67f2c1aeba2198b3
Christian Weichsel <c.weichsel@gmx.de>
2020-11-01 13:33:21 +0100

    added tabels

--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
 #%% Load data
 import pymongo
 import ssl
-
-
+uri = "mongodb://marken:6kwieS9bXJhgqXJL9tvzdl7F4b4s8S37ZssSuhdfb4zQOUzNWYqcRMzPsd4HwxllXlnq1spJJ1J8GAZPoZ69JQ==@marken.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&maxIdleTimeMS=120000&appName=@marken@&retrywrites=false"
+client = pymongo.MongoClient(uri, ssl=True)
 
 db = client.Berlin_West   #Select the database
 collect = db.Container1 #Select the collection
@@ -10,14 +10,16 @@ saetze = db.Satz #Select the collection
 
 
 # %% get data
-Marken = collect.find()
 
 
-mn_group = collect.aggregate([
-    { "$match" : {"Entwertung" : "o"}},
-    { "$group": { "_id": "$MichNr", "total": { "$sum": "$Anzahl" } } },
-])
 
+# mn_group = saetze.aggregate( [ { "$group" : { "_id" : "$Jahrgang" } } ] )
+
+mn_group = saetze.aggregate([
+   { "$group" : { "_id" : "$Jahrgang", "books": { "$push": "$Beschreibung" } } }
+ ])
+
+# %%
 for mn in mn_group:
     print(mn)
  
